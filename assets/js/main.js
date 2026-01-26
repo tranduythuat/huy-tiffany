450783a1b577c4275dfdac3cf2ede7026c52cb48
tranduythuat <thuattran1810@gmail.com>
2026-01-26 18:32:40 +0700

    Init: Huy - Tiffany

---
+++ assets/js/main.js
@@ -0,0 +1,200 @@
+// Kích hoạt ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
+
+// Gọi các hiệu ứng có sẵn
+document.addEventListener("DOMContentLoaded", () => {
+  const mainSwiper = new Swiper(".main-swiper", {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    // thumbs: {
+    //   swiper: thumbSwiper,
+    // },
+    autoplay: {
+      delay: 3000, // thời gian giữa các lần chuyển (ms)
+      disableOnInteraction: false, // không tắt khi người dùng bấm
+    },
+
+    loop: true, // lặp lại ảnh
+    effect: "fade", // hiệu ứng chuyển mượt
+    fadeEffect: { crossFade: true },
+    speed: 1000 // tốc độ chuyển (ms)
+  });
+
+  gsapFlipIn(".animate-flip");
+  gsapFadeIn(".fade-in");
+  gsapFadeRight(".fade-right");
+  gsapFadeLeft(".fade-left");
+  gsapFadeUp(".fade-up");
+  gsapFadeDown(".fade-down");
+  gsapRotateBottomLeft(".rotate-bl");
+  gsapRotateBottomRight(".rotate-br");
+  gsapFlipVerticalLeft(".flip-vertical-left");
+  gsapRollInLeft(".roll-in-left");
+  gsap_rotate_bl__float(".rotate-bl--float");
+
+  const tl_dresscode = gsap.timeline({
+    repeatDelay: 0,  // delay giữa các lần lặp
+    defaults: { duration: .8, ease: "power2.out" }, // giá trị mặc định
+    scrollTrigger: {
+      trigger: ".color-palette",
+      start: "top 90%", // khi phần tử xuất hiện 80% trong viewport
+    }
+  });
+
+  // Thêm các animation theo thứ tự
+  // tl_dresscode.from(".white", { x: -100, opacity: 0 })        // box đỏ bay xuống
+  //   .from(".off-white", { x: -100, opacity: 0 }, "-=0.3")       // box xanh bay từ trái
+  //   .from(".cream-linen", { x: -100, opacity: 0 }, "-=0.3");    // box xanh lá phóng to dần
+
+  async function playMusic(e) {
+    const music = document.getElementById('audio');
+    if (!music.src) {
+        alert('Chưa có nhạc, vui lòng thêm src cho audio.');
+        return;
+    }
+    if (music.paused) {
+      music.play();
+    } 
+    music.addEventListener('play', () => {
+        iconSvg.classList.add('spin');
+    });
+  }
+
+  async function toggleMusic(e) {
+    const audio = document.getElementById('audio');
+    const iconSvg = document.getElementById('iconSvg');
+    if (!audio.src) {
+        alert('Chưa có nhạc, vui lòng thêm src cho audio.');
+        return;
+    }
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+
+    audio.addEventListener('play', () => {
+        iconSvg.classList.add('spin');
+    });
+    audio.addEventListener('pause', () => {
+        iconSvg.classList.remove('spin');
+    });
+  }
+
+  const qrcode = document.getElementById('qr-btn');
+  qrcode.addEventListener("click", toggleQR);
+
+  const btn = document.getElementById('player-btn');
+  btn.addEventListener('click', toggleMusic);
+
+  const form = document.forms["rsvpForm"];
+  if (form) {
+    form.addEventListener("submit", (e) => handleFormSubmit(e));
+  }
+});
+
+function toggleQR(e) {
+  e.preventDefault();
+  Swal.fire({
+      title: "",
+      text: "",
+      imageUrl: "assets/images/couple/IMG_2584.webp",
+      imageWidth: '100%',
+      imageHeight: "auto",
+      imageAlt: "Custom image",
+      html: `
+          <div class="qrcode-box">
+              <div class="item">
+                  <div class="info">
+                      <p>Tên TK: Tiffany Hoang</p>
+                      <p>Số TK: xxxx</p>
+                      <p>Ngân hàng: xxxx</p>
+                  </div>
+                  <div class="qrcode-img">
+                      <img src="assets/images/qrcode.jpeg" alt="">
+                  </div>
+              </div>
+          </div>
+      `,
+      confirmButtonColor: "#dba7b2ff"
+  });
+}
+
+async function handleFormSubmit(e) {
+  e.preventDefault();
+
+  const form = e.target;
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+  console.log("🚀 ~ handleFormSubmit ~ data:", data);
+
+  const {
+    name: name,
+    confirm: confirm,
+    attendance: attendance,
+    dietary: dietary,
+    other: other,
+    wish: wish,
+  } = data;
+  console.log("🚀 ~ handleFormSubmit 2~ data:", data);
+
+  // Thông báo khi bắt đầu gửi
+  Swal.fire({
+    title: 'Đang gửi ...',
+    text: "Vui lòng chờ trong giây lát",
+    icon: "info",
+    allowOutsideClick: false,
+    didOpen: () => {
+      Swal.showLoading();
+    },
+  });
+
+  const url = "?sheet=confirm";
+
+  try {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams({
+        name,
+        confirm,
+        attendance,
+        dietary,
+        other,
+        wish
+      }),
+    });
+
+    const result = await res.json().catch(() => ({}));
+    console.log("Server response:", result);
+
+    form.reset();
+
+    // Thông báo thành công
+    Swal.fire({
+      title: "Thành công!",
+      text: "Cảm ơn bạn đã gửi phản hồi, thông tin đã được gửi đến dâu rể rồi nha",
+      icon: "success",
+      confirmButtonText: "OK",
+      confirmButtonColor: "#000",
+    });
+  } catch (error) {
+    console.error("Error:", error);
+
+    // Thông báo lỗi
+    Swal.fire({
+      title: "Lỗi!",
+      text: "OPPS! Đã xảy ra lỗi: " + error.message,
+      icon: "error",
+      confirmButtonText: "Thử lại",
+      confirmButtonColor: "#000",
+    });
+  }
+}
